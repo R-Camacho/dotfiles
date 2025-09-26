ad1f41ca0eb1930d5a65b76d024e8f21f87ed08b
R-Camacho <rodrigompcamacho@gmail.com>
2025-09-27 00:06:29 +0100

    Fix lsp config for good

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,12 +1,18 @@
 return {
 	{
-		"neovim/nvim-lspconfig",
+		"williamboman/mason-lspconfig.nvim",
 		dependencies = {
 			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
+			"neovim/nvim-lspconfig",
 			"j-hui/fidget.nvim",
 		},
 		config = function()
+			require("fidget").setup({})
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				automatic_enable = true,
+			})
+
 			local blink_cmp = require("blink.cmp")
 			local capabilities = vim.tbl_deep_extend(
 				"force",
@@ -15,39 +21,36 @@ return {
 				blink_cmp.get_lsp_capabilities({}, false)
 			)
 
-			local map = function(mode, lhs, rhs, desc, bufnr)
-				vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
-			end
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(ev)
+					local bufnr = ev.buf
+					local client = vim.lsp.get_client_by_id(ev.data.client_id)
+					if not client then
+						return
+					end
+					local map = function(mode, lhs, rhs, desc)
+						vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+					end
 
-			local on_attach = function(_, bufnr)
-				map("n", "<leader>lca", vim.lsp.buf.code_action, "Show code actions", bufnr)
-				map("n", "<leader>rn", vim.lsp.buf.rename, "Rename symbol", bufnr)
-				map("n", "gd", vim.lsp.buf.definition, "Goto definition", bufnr)
-				map("n", "gi", vim.lsp.buf.implementation, "Goto implementation", bufnr)
-				map("n", "gr", vim.lsp.buf.references, "Goto references", bufnr)
-				map("n", "K", vim.lsp.buf.hover, "Hover information", bufnr)
-				map("n", "<leader>ws", vim.lsp.buf.workspace_symbol, "Workspace symbols", bufnr)
-				map("n", "<leader>d", vim.diagnostic.open_float, "Show diagnostics", bufnr)
+					map("n", "<leader>lca", vim.lsp.buf.code_action, "Show code actions")
+					map("n", "<leader>rn", vim.lsp.buf.rename, "Rename symbol")
+					map("n", "gd", vim.lsp.buf.definition, "Goto definition")
+					map("n", "gi", vim.lsp.buf.implementation, "Goto implementation")
+					map("n", "gr", vim.lsp.buf.references, "Goto references")
+					map("n", "K", vim.lsp.buf.hover, "Hover information")
+					map("n", "<leader>ws", vim.lsp.buf.workspace_symbol, "Workspace symbols")
+					map("n", "<leader>d", vim.diagnostic.open_float, "Show diagnostics")
 
-				map("n", "[d", function()
-					vim.diagnostic.jump({ count = 1, float = true })
-				end, "Previous diagnostic", bufnr)
-				map("n", "]d", function()
-					vim.diagnostic.jump({ count = -1, float = true })
-				end, "Next diagnostic", bufnr)
+                    -- stylua: ignore start
+					map("n", "[d", function() vim.diagnostic.jump({ count = 1, float = true }) end, "Previous diagnostic")
+					map("n", "]d", function() vim.diagnostic.jump({ count = -1, float = true }) end, "Next diagnostic")
+					-- stylua: ignore end
 
-				map("i", "<C-k>", vim.lsp.buf.signature_help, "Signature Help", bufnr)
-			end
-
-			require("fidget").setup({})
-			require("mason").setup()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "clangd" },
-				automatic_installation = true,
+					map("i", "<C-k>", vim.lsp.buf.signature_help, "Signature Help")
+				end,
 			})
 
 			vim.lsp.config("*", {
-				on_attach = on_attach,
 				capabilities = capabilities,
 			})
 
@@ -55,7 +58,6 @@ return {
 			vim.lsp.config("clangd", {
 				cmd = { "clangd", "--background-index" },
 				filetypes = { "c", "cpp", "objc", "objcpp" },
-				on_attach = on_attach,
 				capabilities = capabilities,
 			})
 		end,
