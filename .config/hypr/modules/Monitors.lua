ce8e594aae71ff2cb5b91d10564fe4a74f20f1e6
Rodrigo Camacho <rodrigompcamacho@gmail.com>
2026-09-03 16:26:49 +0100

    hyprland: small monitor tweaks

--- .config/hypr/modules/Monitors.lua
+++ .config/hypr/modules/Monitors.lua
@@ -3,14 +3,12 @@ hl.monitor({
     output = "desc:Thermotrex Corporation TL160ADMP03-0",
     mode = "2560x1600@60",
     position = "auto",
-    scale    = "1.33",
+    scale    = "1.6",
 })
 
 -- Other random monitors
 hl.monitor({
     output = "",
-    mode = "preferred",
-    position = "auto",
-    scale    = "1.0",
+    mode = "preferred"
 })
 
