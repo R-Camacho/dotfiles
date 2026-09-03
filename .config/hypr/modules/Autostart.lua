7805410289a197155e087a7439d84fe3b378b708
Rodrigo Camacho <rodrigompcamacho@gmail.com>
2026-09-03 18:48:51 +0100

    hyprland: minor tweaks

--- .config/hypr/modules/Autostart.lua
+++ .config/hypr/modules/Autostart.lua
@@ -7,8 +7,9 @@ hl.on("hyprland.start", function ()
     hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
 
     hl.exec_cmd("dunst")
+    hl.exec_cmd("nm-applet")
 
     -- clipboard manager
-    hl.exec_cmd("wl-paste --type text --type image --watch cliphist store")
+    hl.exec_cmd("wl-paste --type text --watch cliphist store")
     hl.exec_cmd("wl-paste --type image --watch cliphist store")
 end)
--- .config/hypr/modules/Misc.lua
+++ .config/hypr/modules/Misc.lua
@@ -2,6 +2,7 @@ hl.config({
     misc = {
         force_default_wallpaper = 0,
         disable_hyprland_logo = true,
+        disable_splash_rendering = true,
         enable_swallow = true,
         swallow_regex = "^(Alacritty|kitty)$",
     },
