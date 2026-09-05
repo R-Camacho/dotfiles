97f3acadd1996e9e42e325c6fd58f637ed0eb8e6
R-Camacho <rodrigompcamacho@gmail.com>
2026-09-05 23:13:16 +0100

    hyprland: add ENV variables to hyprland

--- .config/hypr/modules/Misc.lua
+++ .config/hypr/modules/Misc.lua
@@ -11,3 +11,12 @@ hl.config({
 -- ENV variables
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE","24")
+
+hl.env("GDK_BACKEND", "wayland,x11,*")
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+hl.env("SDL_VIDEODRIVER", "wayland")
+hl.env("CLUTTER_BACKEND", "wayland")
+hl.env("OZONE_PLATFORM","wayland")
+hl.env("MOZ_ENABLE_WAYLAND","wayland")
+hl.env("QT_QPA_PLATFORM","wayland")
+hl.env("ELECTRON_OZONE_PLATFORM_HINT","auto")
