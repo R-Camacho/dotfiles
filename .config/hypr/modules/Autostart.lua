55868ecac40de96c3dd68c0ac65354e53ca1087c
Rodrigo Camacho <rodrigompcamacho@gmail.com>
2026-09-07 10:30:32 +0100

    Switch dunst for mako

--- .config/hypr/modules/Autostart.lua
+++ .config/hypr/modules/Autostart.lua
@@ -6,7 +6,7 @@ hl.on("hyprland.start", function ()
 
     hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
 
-    hl.exec_cmd("dunst")
+    hl.exec_cmd("mako")
     hl.exec_cmd("nm-applet")
 
     -- clipboard manager
